bf4d9fb846cdd7fe0565276660638a8edd2000a1
vicc <vicc1997@gmail.com>
2022-08-30 20:27:44 -0400

    2.16

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,39 +1,44 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
+import personService from "./services/persons";
 
 const App = () => {
+  const [personsAll, setPersonsAll] = useState([]);
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:3001/persons").then((response) => setPersons(response.data));
-  });
-
-  const handleNameChange = (event) => setNewName(event.target.value);
-  const handleNumberChange = (event) => setNewNumber(event.target.value);
-  const handleSearchChange = (event) => setSearch(event.target.value);
+    personService.getAll().then((initialPersons) => {
+      setPersonsAll(initialPersons);
+    });
+  }, []);
 
   const addNewPerson = (event) => {
     event.preventDefault();
+
     if (persons.some((person) => person.name === newName)) {
       return alert(`${newName} is already added to phonebook`);
     }
 
     const newPerson = { name: newName, number: newNumber };
 
-    axios.post("http://localhost:3001/persons", newPerson).then((response) => {
-      setPersons(persons.concat(response.data));
+    personService.create(newPerson).then((returnedPerson) => {
+      setPersonsAll(personsAll.concat(returnedPerson));
       setNewName("");
       setNewNumber("");
     });
   };
 
-  const personsToShow = persons.filter((person) => person.name.toLowerCase().includes(search));
+  const handleNameChange = (event) => setNewName(event.target.value);
+  const handleNumberChange = (event) => setNewNumber(event.target.value);
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+    setPersons(personsAll.filter((person) => person.name.toLowerCase().includes(event.target.value)));
+  };
 
   return (
     <div>
@@ -48,7 +53,7 @@ const App = () => {
         handleNumberChange={handleNumberChange}
       />
       <h2>Numbers</h2>
-      <Persons personsToShow={personsToShow} />
+      <Persons personsToShow={persons} />
     </div>
   );
 };
