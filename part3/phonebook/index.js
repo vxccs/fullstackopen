cc19085134fef0f792c81ce1f4709f54f561f6b2
vxccs <vicc1997@gmail.com>
2023-01-30 18:43:04 -0400

    part3

---
+++ part3/phonebook/index.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+app.get('/info/', (req, res) => {
+  res.send(`
+    <p>phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `);
+});
+
+app.get('/api/persons/', (req, res) => {
+  res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((p) => p.id === id);
+
+  if (!person) res.status(404).end();
+
+  res.json(person);
+});
+
+app.post('/api/persons', (req, res) => {
+  const { body } = req;
+
+  if (!body.name || !body.number) return res.status(400).json({ error: 'missing content' });
+  if (persons.some((p) => p.name === body.name)) return res.status(400).json({ error: 'name must be unique' });
+
+  const newPerson = {
+    id: Math.floor(Math.random() * 10000),
+    name: body.name,
+    number: body.number,
+  };
+  persons = [...persons, newPerson];
+  res.json(newPerson);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((p) => p.id !== id);
+
+  res.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
