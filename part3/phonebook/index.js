81e85a77f2099f7c29a1aa26b7247f45bc29cd0c
vxccs <vicc1997@gmail.com>
2023-01-30 19:34:38 -0400

    uibuild

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(express.static('build'));
 app.use(cors());
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
