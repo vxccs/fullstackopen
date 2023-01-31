da3811884f6bb02773be143edc783e2d48747d91
vxccs <vicc1997@gmail.com>
2023-01-31 15:09:26 -0400

    3.18

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,11 +3,10 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
-app.use(express.json());
-app.use(express.static('build'));
-app.use(cors());
-
 const Person = require('./models/person');
+app.use(express.static('build'));
+app.use(express.json());
+app.use(cors());
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(
@@ -17,19 +16,22 @@ app.use(
 // get info
 app.get('/info/', (req, res) => {
   Person.countDocuments({}, (err, count) => {
-    res.send(`<p>Phonebook has info for ${count} people</p>
-              <p>${new Date()}</p>`);
+    res.send(
+      `<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`
+    );
   });
 });
 
 // get all items
-app.get('/api/persons/', (req, res) => {
+app.get('/api/persons/', (req, res, next) => {
   Person.find({}).then((resu) => res.json(resu));
 });
 
 // get single item
-app.get('/api/persons/:id', (req, res) => {
-  Person.findById(req.params.id).then((person) => res.json(person));
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => (person ? res.json(person) : res.status(404).end()))
+    .catch((e) => next(e));
 });
 
 // create new item
@@ -48,12 +50,41 @@ app.post('/api/persons', (req, res) => {
   person.save().then((person) => res.json(person));
 });
 
-// app.delete('/api/persons/:id', (req, res) => {
-//   const id = Number(req.params.id);
-//   persons = persons.filter((p) => p.id !== id);
+// delete an item
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(() => res.status(204).end())
+    .catch((e) => next(e));
+});
 
-//   res.status(204).end();
-// });
+// update an item
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = req.body;
+
+  Person.findByIdAndUpdate(req.params.id, { name, number }, { new: true })
+    .then((person) => res.json(person))
+    .catch((e) => next(e));
+});
+
+// 404 middleware
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+// error middleware
+const errorHandler = (e, req, res, next) => {
+  console.error(e.message);
+
+  if (e.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(e);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
