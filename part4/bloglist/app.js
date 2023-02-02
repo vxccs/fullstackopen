6734b252b9c28f7f2db7f8cef0e7b409c14096c0
vxccs <vicc1997@gmail.com>
2023-02-02 10:21:52 -0400

    4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,32 @@
+const config = require('./utils/config');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
+
+mongoose.set('strictQuery', false);
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => logger.info('connected to MongoDB'))
+  .catch((e) => logger.error('error connecting to MongoDB:', e.message));
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
