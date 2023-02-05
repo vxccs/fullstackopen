3554745463faa4f5e386e6da21c9dc20ea228f0d
vxccs <vicc1997@gmail.com>
2023-02-05 12:02:44 -0400

    4.16

---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,36 @@
+const usersRouter = require('express').Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
+});
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body;
+
+  if (!username || !password) {
+    return response.status(400).json({ error: 'missing fields' });
+  }
+
+  const exists = await User.findOne({ username });
+  if (exists) {
+    return response.status(400).json({ error: 'username must be unique' });
+  }
+
+  if (password.length < 3) {
+    return response.status(400).json({ error: 'password must be at least 3 characters' });
+  }
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({ username, name, passwordHash });
+
+  const savedUser = await user.save();
+
+  response.status(201).json(savedUser);
+});
+
+module.exports = usersRouter;
