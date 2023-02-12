32d9d16de4c447cd43a6ed6b89b4cd24d345efdd
vxccs <vicc1997@gmail.com>
2023-02-12 16:36:41 -0400

    7.3

---
+++ part7/anecdotes-routed/src/components/CreateNew.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const CreateNew = ({ addNew, showNotification }) => {
+  const [content, setContent] = useState('');
+  const [author, setAuthor] = useState('');
+  const [info, setInfo] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addNew({
+      content,
+      author,
+      info,
+      votes: 0,
+    });
+    showNotification(`'${content}' has been added`);
+    setContent('');
+    setAuthor('');
+    setInfo('');
+    navigate('/');
+  };
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input
+            name="content"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+        <div>
+          author
+          <input
+            name="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+        </div>
+        <div>
+          url for more info
+          <input
+            name="info"
+            value={info}
+            onChange={(e) => setInfo(e.target.value)}
+          />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateNew;
---
+++ part7/anecdotes-routed/src/components/Notification.jsx
@@ -0,0 +1,6 @@
+const Notification = ({ notification }) => {
+  if (!notification) return null;
+  return <div>{notification}</div>;
+};
+
+export default Notification;
