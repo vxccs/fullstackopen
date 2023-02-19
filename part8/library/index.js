86f48d5830a87008784b27a8756bbe146d2e0762
vxccs <vicc1997@gmail.com>
2023-02-19 14:30:24 -0400

    8.4

--- part8/library/index.js
+++ part8/library/index.js
@@ -115,7 +115,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -124,7 +124,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (args.author) {
+        return books.filter((b) => b.author === args.author);
+      }
+      return books;
+    },
     allAuthors: () => authors,
   },
   Author: {
